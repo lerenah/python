4e01641b37698ff7c213f2ae74b083e9766279ce
Lerena Holloway <holloway.lerena@gmail.com>
2019-05-23 22:29:20 -0400

    completes Linked List class

--- python_challenges.py
+++ python_challenges.py
@@ -7,5 +7,26 @@ class Linked_List:
   def get_next(self):
     return self.next
 
-  
+  def set_next(self, node):
+    self.next = node
+
+  def has_next(self):
+    if self.next != None:
+      return True
+    else:
+      return False
+
+  def get_down(self):
+    return self.down
+
+  def set_down(self, node):
+    self.down = node
+
+  def has_down(self):
+    if self.down != None:
+      return True
+    else:
+      return False
+
+
 
