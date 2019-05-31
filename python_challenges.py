ceb99d9a135b362651dececf6e2f1ea725a245e9
Lerena Holloway <holloway.lerena@gmail.com>
2019-05-31 18:00:43 -0400

    updates stock class

--- python_challenges.py
+++ python_challenges.py
@@ -329,26 +329,33 @@ class StockPrices:
         self.stocks = []
 
     def addPrice(self, stock, price):
+        counter = 0
         if price <= 0:
             return 'Price must be a positive integer.'
-        for name in self.stocks:
-            if stock in name.keys():
-                self.stocks[name].append(price)
-            else:
-                self.stocks.append({stock: [price]})
+
+        while counter < len(self.stocks):
+            if stock == self.stocks[counter]['name']:
+                self.stocks[counter]['price'].append(price)
+                return
+            counter += 1
+
+        self.stocks.append({'name': stock, 'price': [price]})
 
     def getPrices(self, stock, n):
         if n <= 0:
             return 'The number of prices requested must be a positive integer.'
-        for name in self.stocks:
-            if stock in name.keys():
-                total_stocks = len(self.stocks[name])
+        for el in self.stocks:
+            if stock == el['name']:
+                total_stocks = len(el['price'])
                 if n in range(total_stocks):
-                    return self.stocks[name][:n + 1]
-                else:
-                    return 'This stock has {} prices.  How many of those would you like to see?'.format(total_stocks)
+                    return el['price'][:n]
+                elif n > len(el['price']):
+                    return el['price']
             else:
-                return 'This stock is not yet in our listing.'
+                continue
+
+        else:
+            return 'This stock is not yet in our listing.'
 
 
 
