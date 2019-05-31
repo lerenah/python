192b7860dcff8ea8379134ceddee2725ffef8624
Lerena Holloway <holloway.lerena@gmail.com>
2019-05-31 12:53:09 -0400

    adds TicTacToe class

--- python_challenges.py
+++ python_challenges.py
@@ -258,6 +258,43 @@ def safe_steps(web):
 
 print(safe_steps(matrix))
 
+'''
+Tic Tac Toe Game
+'''
+class TicTacToe:
+    def __init__(self):
+        self.positions = {}
+        self.moves = []
+        self.board = []
+        self.make_board()
+
+    def make_board(self):
+        for i in range(3):
+            row = []
+            for j in range(3):
+                row.append((i,j))
+            self.board.append(row)
+        return self.board
+
+    def access_move(self, n):
+        for k in self.positions.keys():
+            if k == self.moves[n - 1]:
+                return 'The {}th move was an {} at {}'.format(n, self.positions[k], k)
+
+    def record_move(self, value, pos):
+        self.moves.append(pos)
+        self.positions[pos] = value
+
+    def make_move(self, value, pos):
+        # update board
+        self.board[pos[0]][pos[1]] = value
+        # record move
+        self.record_move(value, pos)
+        return self.board
+
+
+
+game1 = TicTacToe()
 
 
 
