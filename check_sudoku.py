c0b45f41716cd554008230ecbbfa0de53f1e7721
Lerena Holloway <holloway.lerena@gmail.com>
2019-06-01 21:51:37 -0400

    adds sudoku challenge

---
+++ check_sudoku.py
@@ -0,0 +1,43 @@
+correct = [[1, 2, 3],
+           [2, 3, 1],
+           [3, 1, 2]]
+
+incorrect = [[1, 2, 3, 4],
+             [2, 3, 1, 3],
+             [3, 1, 2, 3],
+             [4, 4, 4, 4]]
+
+incorrect2 = [[1, 2, 3, 4],
+              [2, 3, 1, 4],
+              [4, 1, 2, 3],
+              [3, 4, 1, 2]]
+
+incorrect3 = [[1, 2, 3, 4, 5],
+              [2, 3, 1, 5, 6],
+              [4, 5, 2, 1, 3],
+              [3, 4, 5, 2, 1],
+              [5, 6, 4, 3, 2]]
+
+incorrect4 = [['a', 'b', 'c'],
+              ['b', 'c', 'a'],
+              ['c', 'a', 'b']]
+
+incorrect5 = [[1, 1.5],
+              [1.5, 1]]
+
+
+# Define a function check_sudoku() here:
+def check_sudoku(puzzle):
+  cols = []
+  rows = []
+  for row in puzzle:
+    cols.append(row[0])
+    rows.append(row)
+  if set(cols) != cols:
+    return False
+  if set(rows) != row:
+    return False
+  return True
+
+
+print(check_sudoku(incorrect))
