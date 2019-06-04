9ad52297079c26aafa6eee16dc456c91f7081e7d
Lerena Holloway <holloway.lerena@gmail.com>
2019-06-03 20:18:26 -0400

    adds challenge

---
+++ browser.py
@@ -0,0 +1,19 @@
+stack = []
+
+def history(url):
+    if len(stack) > 5:
+        stack.remove(stack[0])
+        stack.append(url)
+    elif url in stack:
+        stack.remove(url)
+        stack.append(url)
+    else:
+        stack.append(url)
+
+def retrieve_history():
+    for idx, url in enumerate(stack[::-1]):
+        print((idx + 1), url)
+
+
+def clear_history():
+    stack = []
