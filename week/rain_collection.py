7497c31d10d8c7e188303445ff1624f5ded83f2e
Lerena Holloway <holloway.lerena@gmail.com>
2019-06-07 19:07:25 -0400

    adds rain collection

---
+++ week/rain_collection.py
@@ -0,0 +1,47 @@
+def lesser_level(a, b):
+    if a < b:
+        return a
+    elif b < a:
+        return b
+    else:
+        return a
+
+
+def second_highest(arr):
+    maxed = arr[0]
+    for el in arr:
+        if el > maxed:
+            maxed = el
+    newArr = arr[:arr.index(maxed)]
+    return maxed, newArr
+
+
+def rain_collector(set_):
+    highest = max(set_)
+    valley = False
+    contents = []
+    water = 0
+    for idx, num in enumerate(set_):
+        i = idx + 1
+        if i in range(len(set_)):
+            if set_[i] < num:
+                valley = True
+                contents = []
+                while valley and i in range(len(set_)):
+                    if num == highest:
+                        final, new_arr = second_highest(set_[set_.index(highest) + 1:])
+                        for el in new_arr:
+                            contents.append(el)
+                        break
+                    if set_[i] >= num:
+                        final = set_[i]
+                        break
+                    contents.append(set_[i])
+                    i += 1
+            if valley and final != 0:
+                levels = lesser_level(num, final)
+                for el in contents:
+                    water += (levels - el)
+                valley = False
+                final = 0
+    return water
