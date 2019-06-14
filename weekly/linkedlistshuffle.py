bcb3144169400fe3ade48d7389aee4ea786226cf
Lerena Holloway <holloway.lerena@gmail.com>
2019-06-14 16:16:53 -0400

    updates list shuffle

--- weekly/linkedlistshuffle.py
+++ weekly/linkedlistshuffle.py
@@ -1,43 +1,62 @@
 import random
 
-def random_display(self):
-    elems = []
-    names = []
-    randomed = []
-    if self.head.value is None:
-        self.head = self.head.next
-    curr = self.head
+class LLNode:
+    def __init__(self, value=None):
+        self.value = value
+        self.next = None
 
-    while curr.next:
-        prev = curr
-        curr = curr.next
-        prev.next = None
-        elems.append(prev.value)
-        names.append(prev)
-    elems.append(curr.value)
-    names.append(curr)
-    if None in elems:
-        elems.remove(None)
-    print(elems)
-    print(self.head.value, ' is head')
+class LL:
+    def __init__(self):
+        self.head = LLNode()
 
-    def pick_random():
-        _random = random.choice(names)
-        names.remove(_random)
-        return _random
+    def add_node(self, value):
+        new_node = LLNode(value)
+        curr = self.head
+        while curr.next != None:
+            curr = curr.next
+        curr.next = new_node
 
-    chosen = []
-    el = names[0]
-    names.remove(el)
-    self.head = el
-    randomed.append(self.head)
-    while len(names):
-        choose = pick_random()
-        if choose not in chosen:
-            if el.value != choose.value:
-                el.next = choose
-            chosen.append(choose)
-            randomed.append(el)
-            print(el.value)
-            print(el.next.value)
+    def length(self):
+        curr = self.head
+        total = 0
+        while curr.next:
+            total+=1
+            curr = curr.next
+        return total
+
+    def display(self):
+        elems = []
+        curr_node = self.head
+        while curr_node.next:
+            if curr_node is not None:
+                elems.append(curr_node.value)
+                curr_node = curr_node.next
+        elems.append(curr_node.value)
+
+
+        print(elems)
+
+    def list_shuffle(self):
+        names = []
+        if self.head.value is None:
+            self.head = self.head.next
+        curr = self.head
+
+        while curr.next:
+            prev = curr
+            curr = curr.next
+            prev.next = None
+            names.append(prev)
+        names.append(curr)
+
+        def pick_random():
+            _random = random.choice(names)
+            names.remove(_random)
+            return _random
+
+        el = names[0]
+        names.remove(el)
+        while len(names):
+            choose = pick_random()
+            el.next = choose
             el = el.next
