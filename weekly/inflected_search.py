b362eb44b169124922b3a77e7a9c98938ae6bbc5
Lerena Holloway <holloway.lerena@gmail.com>
2019-06-17 12:38:26 -0400

    adds inflection challenge

---
+++ weekly/inflected_search.py
@@ -0,0 +1,31 @@
+def search(arr, n):
+    idx = -1
+    left = 0
+    right = len(arr) - 1
+    mid = len(arr) // 2
+
+    if n == arr[mid]:
+        return mid
+
+    # check for asc sort
+    elif arr[left] < arr[mid] < arr[right]:
+        if n < arr[mid]:
+            return search(arr[:mid], n)
+        elif n > arr[mid]:
+            return search(arr[mid + 1:], n)
+
+    # check for desc sort
+    elif arr[left] > arr[mid] > arr[right]:
+        if n < arr[mid]:
+            return search(arr[mid + 1:], n)
+        elif n > arr[mid]:
+            return search(arr[:mid], n)
+
+    # check for inflection
+    elif arr[left] > arr[mid] < arr[right]:
+        if n < arr[mid]:
+            return search(arr[:mid], n)
+        else:
+            return search(arr[mid + 1:], n)
+
+    return idx
