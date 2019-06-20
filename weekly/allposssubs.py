6c2edd667a1f842ee4cd20ae1325f2db1a0efb58
Lerena Holloway <holloway.lerena@gmail.com>
2019-06-20 13:11:23 -0400

    updates sets challenge

--- weekly/allposssubs.py
+++ weekly/allposssubs.py
@@ -5,12 +5,18 @@ subs = []
 
 def sets(arr):
   for el in arr:
-     subs.append(el)
+    subs.append(el)
 
   if len(subs) == len(arr):
     newArr = []
     for i in subs:
       s = list(filter(lambda x: x != i, arr))
+      for el in s:
+        x = [i, el]
+        z = sorted(x)
+        y = ''.join(z)
+        if y not in newArr:
+          newArr.append(i + el)
       s = ''.join(s)
       newArr.append(s)
 
@@ -20,3 +26,4 @@ def sets(arr):
 
 
   return subs
+
