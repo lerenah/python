c4371c701967b6a04edae06b1686d779773c63e6
Lerena Holloway <holloway.lerena@gmail.com>
2019-06-21 13:34:06 -0400

    updates challenge

--- weekly/candy_crush.py
+++ weekly/candy_crush.py
@@ -2,12 +2,10 @@ str = 'ABCCCBB'
 
 def crush_it(str):
     i = 0
-    while i in range(len(str) - 1):
+    while i in range(len(str)) and i + 2 in range(len(str)):
         if str[i] == str[i + 1] and str[i] == str[i + 2]:
             str = str.replace(str[i], '')
             i = 0
             continue
         i += 1
     return str
-
-print(crush_it(str))
