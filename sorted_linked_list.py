2743307ddb60d9cc599623b406fa1952446368db
Lerena Holloway <holloway.lerena@gmail.com>
2019-06-22 17:21:47 -0400

    updates sorted ll

--- sorted_linked_list.py
+++ sorted_linked_list.py
@@ -8,6 +8,17 @@ class LinkedList:
     def __init__(self, node):
         self.head = node
 
+
+    def change_next(self, node):
+        node.next = None
+        if not self.head:
+            self.head = node
+        else:
+            curr = self.head
+            while curr.next:
+                curr = curr.next
+            curr.next = node
+
     def insert_node(self, node):
         if not self.head:
             self.head = node
@@ -26,48 +37,19 @@ class LinkedList:
 
     def sort(self):
         if self.head:
+            nodes = []
             curr = self.head
-            min = curr
 
-            lft_of_min = curr
-            old_prev = curr
-
-            while curr.next:
-                # check for min value first to set as head
-                if curr.next.value < min.value:
-                    min = curr.next
-                    lft_of_min = curr
-                # traverse and swap the rest of the list
-                elif curr.next.value < curr.value:
-                    temp = curr.next
-                    prev = curr
-                    curr = temp
-                    prev.next = curr.next
-                    curr.next = prev
-                    old_prev.next = curr
-
-                old_prev = curr
+            while curr:
+                nodes.append(curr)
                 curr = curr.next
 
-            if min.next is None:
-                min.next = self.head
-                lft_of_min.next = None
-                self.head = min
-            else:
-                lft_of_min.next = min.next
-                min.next = self.head
-                self.head = min
+            nodes = sorted(nodes, key=lambda node: node.value)
+            nodes[0].next = None
+            newList = LinkedList(nodes[0])
 
+            for node in nodes[1:]:
+                newList.change_next(node)
 
-
-
-node = Node(2)
-ll = LinkedList(node)
-ll.insert_node(Node(5))
-ll.insert_node(Node(4))
-ll.insert_node(Node(7))
-ll.insert_node(Node(1))
-
-ll.print_list()
-ll.sort()
-ll.print_list()
+            return newList.print_list()
+        return
