7fff591742383784a107026963a178223a2292b1
Lerena Holloway <holloway.lerena@gmail.com>
2019-06-23 13:43:23 -0400

    updates palindrome challenge

--- weekly/palindrome_tracker.py
+++ weekly/palindrome_tracker.py
@@ -6,27 +6,34 @@ class PalTracker:
         self.increment = -1
         self.palindrome = False
 
-    def track_palindrome(self):
-        if not self.palindrome:
-            self.isPalindrome()
-
-
-
     def track(self, char):
         self.storage.append(char)
         self.str += char
         self.increment += 1
-        self.track_palindrome()
+        self.is_palindrome()
 
 
-    def isPalindrome(self):
-        if len(self.storage):
-            first = self.storage[self.increment]
+    def is_palindrome(self):
+        if not len(self.storage):
+            return False
+        else:
+            first = self.storage[0]
             self.palindrome = True
-        if self.increment + 1 in range(len(self.storage)):
-            mover = self.increment + 1
-            if first == self.storage[mover]:
-                self.palindrome = True
-
+            i = 0
+            mid = len(self.storage) // 2
+            while i in range(len(self.storage)) and (mid + i) in range(len(self.storage)):
+                next_letter = self.storage[self.increment]
+                if first != next_letter:
+                    self.palindrome = False
+                    return self.palindrome
+                else:
+                    left_of_mid = mid - i
+                    right_of_mid = mid + i
+                    if self.storage[left_of_mid] != self.storage[right_of_mid]:
+                        self.palindrome = False
+                        return self.palindrome
+                    i += 1
 
         return self.palindrome
+
+
