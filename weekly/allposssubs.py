b8b1522bb4f84be95668999beba33f470f6bae5e
Lerena Holloway <holloway.lerena@gmail.com>
2019-06-23 21:21:32 -0400

    updates all possible subsets

--- weekly/allposssubs.py
+++ weekly/allposssubs.py
@@ -1,30 +1,13 @@
-arr = ['a', 'b', 'c']
-# look at first el in arr
-
 subs = []
+def subSets(soFar, rest):
+  if rest == '':
+    subs.append(list(soFar))
+    print(soFar)
+    return soFar
+  else:
+    # include it
+    subSets(soFar + rest[0], rest[1:])
+    # exclude it
+    subSets(soFar, rest[1:])
 
-def sets(arr):
-  for el in arr:
-    subs.append(el)
-
-  if len(subs) == len(arr):
-    newArr = []
-    for i in subs:
-      s = list(filter(lambda x: x != i, arr))
-      for el in s:
-        x = [i, el]
-        z = sorted(x)
-        y = ''.join(z)
-        if y not in newArr:
-          newArr.append(i + el)
-      s = ''.join(s)
-      if s not in newArr:
-          newArr.append(s)
-
-  subs.extend(newArr)
-  subs.append([])
-  subs.append(''.join(arr))
-
-
-  return subs
 
