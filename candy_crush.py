2c91b67ea3be894d2684426388e19f875885d117
Lerena Holloway <holloway.lerena@gmail.com>
2019-06-28 17:00:16 -0400

    updates candy crush

--- candy_crush.py
+++ candy_crush.py
@@ -4,7 +4,7 @@ def crush_it(str):
     i = 0
     while i in range(len(str)) and i + 2 in range(len(str)):
         if str[i] == str[i + 1] and str[i] == str[i + 2]:
-            str = str.replace(str[i], '')
+            str = str[:i] + str[i + 3:]
             i = 0
             continue
         i += 1
