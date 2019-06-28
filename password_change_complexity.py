f964d038d206c1982a491a894cddf1a42c865ef7
Lerena Holloway <holloway.lerena@gmail.com>
2019-06-28 17:23:18 -0400

    updates password challenge

--- password_change_complexity.py
+++ password_change_complexity.py
@@ -1,8 +1,12 @@
 def routine_check(str1, str2):
     count = 0
+    diff = 0
+    last_letter = ''
     for char in str1:
-        if char not in str2:
-            count += 1
-    return count
+        if char not in last_letter:
+            count = str1.count(char)
+            num = str2.count(char)
+            diff += count - num
+        last_letter += char
 
-print(routine_check('xxxx', 'yyyy'))
+    return diff
