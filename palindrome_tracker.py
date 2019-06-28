1d3e849b424d80b7f4b3562abebcfc2d431951d9
Lerena Holloway <holloway.lerena@gmail.com>
2019-06-28 17:28:24 -0400

    updates palindrome tracker

--- palindrome_tracker.py
+++ palindrome_tracker.py
@@ -17,22 +17,15 @@ class PalTracker:
         if not len(self.storage):
             return False
         else:
-            first = self.storage[0]
-            self.palindrome = True
             i = 0
-            mid = len(self.storage) // 2
-            while i in range(len(self.storage)) and (mid + i) in range(len(self.storage)):
-                next_letter = self.storage[self.increment]
-                if first != next_letter:
-                    self.palindrome = False
-                    return self.palindrome
+            j = len(self.storage) - 1
+            self.palindrome = True
+            while i <= j:
+                if self.storage[i] != self.storage[j]:
+                    return False
                 else:
-                    left_of_mid = mid - i
-                    right_of_mid = mid + i
-                    if self.storage[left_of_mid] != self.storage[right_of_mid]:
-                        self.palindrome = False
-                        return self.palindrome
                     i += 1
+                    j -= 1
 
         return self.palindrome
 
