9d28a1967d4df58d4046071eb11da0cfb9e6948e
Lerena Holloway <holloway.lerena@gmail.com>
2019-07-01 18:47:09 -0400

    updates challenge

--- weekly/decimal_to_base.py
+++ weekly/decimal_to_base.py
@@ -1,17 +1,15 @@
 hex_conversions = {10: 'A', 11: 'B', 12: 'C', 13: 'D', 14: 'E', 15: 'F'}
 
-def convert_any_base(n, base):
+
+def convert_any_base(n, case):
     num = []
+    base = len(case)
+    if base == 1:
+        return case * n
     negative = False
     if n < 0:
         negative = True
         n = abs(n)
-    if base == "0123456789ABCDEF":
-        base = 16
-    elif base == "01":
-        base = 2
-    elif base == "QWERTY":
-        base = 6
     while n > 0:
         rem = n % base
         if rem > 9 and base == 16:
@@ -22,4 +20,4 @@ def convert_any_base(n, base):
     num = list(reversed(num))
     if negative:
         return '-' + ''.join(num)
-    return "".join(num)
+    return "".join(num).strip("0")
