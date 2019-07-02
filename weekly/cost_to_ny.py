88a916e536328bc6ebac679c8f6c6e9932bdcd69
Lerena Holloway <holloway.lerena@gmail.com>
2019-07-02 16:45:57 -0400

    updates costs challenge

--- weekly/cost_to_ny.py
+++ weekly/cost_to_ny.py
@@ -1,11 +1,24 @@
 def cost_to_fly(coders):
     to_ny = []
     to_sf = []
+    total_ny = 0
+    total_sf = 0
     for ny, sf in coders:
         if ny > sf:
             if len(to_sf) < (len(coders) // 2):
                 to_sf.append(sf)
+                total_sf += sum(to_sf)
+            else:
+                total_ny += sf
+                total_sf += sf
+                if total_ny > total_sf:
+                    total_ny -= sf
+                    to_sf.append(sf)
+                    total_sf -= sf
         else:
             to_ny.append(ny)
+            total_ny += sum(to_ny)
 
-    return sum(to_ny) + sum(to_sf)
+    total = sum(to_ny) + sum(to_sf)
+
+    return total
