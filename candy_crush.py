f963c0776ec31816b14c6ae7b65cfb88fb43fe6a
Lerena Holloway <holloway.lerena@gmail.com>
2019-07-19 13:29:24 -0400

    updates candy crush

--- candy_crush.py
+++ candy_crush.py
@@ -1,11 +1,16 @@
-str = 'ABCCCBB'
-
 def crush_it(str):
     i = 0
-    while i in range(len(str)) and i + 2 in range(len(str)):
-        if str[i] == str[i + 1] and str[i] == str[i + 2]:
-            str = str[:i] + str[i + 3:]
+    j = i + 1
+    while i in range(len(str)) and j + 1 in range(len(str)):
+        if str[i] == str[j] and str[j] == str[j + 1]:
+            start = i
+            while str[i] == str[j]:
+                i += 1
+                j += 1
+            str = str[:start] + str[j:]
             i = 0
+            j = i + 1
             continue
         i += 1
+        j += 1
     return str
