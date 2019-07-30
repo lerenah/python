b504fedfc5a1e99cec655a7591ca90b2e49c84ac
Lerena Holloway <holloway.lerena@gmail.com>
2019-07-29 20:15:21 -0400

    adds reverse challenge

---
+++ weekly/reverse_words.py
@@ -0,0 +1,16 @@
+def reverse_words(message):
+    if len(message) == 1:
+        return message
+    l = 0
+    r = len(message) - 1
+    words = message.count(' ')
+    set_spaces = {}
+    while l != r:
+        swap(message, l, r)
+        l += 1
+        r -= 1
+    curr = 0
+    for i in range(len(message) + 1):
+        if i == len(message) or message[i] == ' ':
+            reverse(message, curr, i - 1)
+            curr = i + 1
