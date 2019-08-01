8f6c2d05ac59010cd192e49bc42d46005aaefcac
Lerena Holloway <holloway.lerena@gmail.com>
2019-08-01 13:28:52 -0400

    adds max_sub challenge

---
+++ weekly/max_sub_array.py
@@ -0,0 +1,18 @@
+def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        counter = nums[0]
+        highest_seen = nums[0]
+        for end in range(1, len(nums)):
+            # check two conditions here; a is higher or a + b is higher
+            if nums[end] > nums[end] + counter:
+                counter = nums[end]
+            else:
+                counter += nums[end]
+            # update highest seen
+            if highest_seen < counter:
+                highest_seen = counter
+
+        return highest_seen
