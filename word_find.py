19c96e48bb92e8675c13b23b573709ebc05811e4
Lerena Holloway <holloway.lerena@gmail.com>
2019-08-04 16:44:47 -0400

    updates word search

--- word_find.py
+++ word_find.py
@@ -4,53 +4,52 @@
 # check for membership of first letter (if word[idx] in col)
 # check to see if neighbors == word[idx + 1]
 
-def get_neighbors(idx, index, letter, arr):
-    if (idx + 1) in range(len(arr)):
-        if arr[idx + 1][index] == letter:
-            print(letter)
-            return idx + 1, index
-    if (idx - 1) in range(len(arr)):
-        if arr[idx - 1][index] == letter:
-            print(letter)
-            return idx - 1, index
-    if (index - 1) in range(len(arr[0])):
-        if arr[idx][index - 1] == letter:
-            print(letter)
-            return idx, index - 1
-    if (index + 1) in range(len(arr[0])):
-        if arr[idx][index + 1] == letter:
-            print(letter)
-            return idx, index + 1
 
-    else:
-        print('False')
-        return -1, -1
+def exist(self, arr, word):
+    """
+    :type board: List[List[str]]
+    :type word: str
+    :rtype: bool
+    """
+    # make another grid
+    grid = [[False] * len(arr[0]) for i in range(len(arr))]
 
+    def get_neighbors(row, col, letter):
+        if 0 <= row + 1 < len(arr):
+            if arr[row + 1][col] == letter and not grid[row + 1][col]:
+                yield row + 1, col
+        if 0 <= row - 1 < len(arr):
+            if arr[row - 1][col] == letter and not grid[row - 1][col]:
+                yield row - 1, col
+        if 0 <= col + 1 < len(arr[0]):
+            if arr[row][col + 1] == letter and not grid[row][col + 1]:
+                yield row, col + 1
+        if 0 <= col - 1 < len(arr[0]):
+            if arr[row][col - 1] == letter and not grid[row][col - 1]:
+                yield row, col - 1
 
-def word_find(arr, str):
-    count = 0
-    for idx, row in enumerate(arr):
-        if str[count] in row:
-            for index, col in enumerate(row):
-                if col in str and col == str[count]:
-                    letter = str[count + 1]
-                    if not get_neighbors(idx, index, letter, arr):
-                        continue
-                    else:
-                        print(str[count])
-                        mod_idx, mod_index = get_neighbors(idx, index, letter, arr)
-                        reset = count
-                        count += 1
-                        while (mod_idx or mod_index) and len(str) - 1 >= count:
-                            if count + 1 in range (len(str)):
-                                mod_idx, mod_index = get_neighbors(mod_idx, mod_index, str[count+1], arr)
-                                if mod_idx == -1 and mod_index == -1:
-                                    count = reset
-                                    break
-                                elif count == len(str) - 1:
-                                    return True
-                                else:
-                                    count += 1
+    def search(row, col, i):
+        print(row, col, word[i])
+        if i == len(word):
+            return True
+        if i == len(word) - 1:
+            return arr[row][col] == word[i]
+        if arr[row][col] != word[i]:
+            return False
+        for neighbors in get_neighbors(row, col, word[i + 1]):
+            n_row, n_col = neighbors
+            grid[n_row][n_col] = True
+            if search(n_row, n_col, i + 1):
+                return True
+            grid[n_row][n_col] = False
 
+        return False
 
+    for row in range(len(arr)):
+        for col in range(len(arr[0])):
+            grid[row][col] = True
+            if search(row, col, 0):
+                return True
+            grid[row][col] = False
 
+    return False
