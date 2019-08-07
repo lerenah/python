1d3786c7ca23fb02fcb0875ef37f4d2b4161d050
Lerena Holloway <holloway.lerena@gmail.com>
2019-08-07 08:13:15 -0400

    updates

--- weekly/kth_smallest_bst.py
+++ weekly/kth_smallest_bst.py
@@ -1,20 +1,40 @@
 class Solution(object):
+
+    # faster
     def kthSmallest(self, root, k):
         """
         :type root: TreeNode
         :type k: int
         :rtype: int
         """
-        import heapq
         collection = []
 
         def helper(node):
             if node is None:
                 return
             helper(node.left)
-            helper(node.right)
             collection.append(node.val)
-
+            helper(node.right)
         helper(root)
-        heapq.heapify(collection)
-        return heapq.nsmallest(k, collection)[-1]
+        return collection[k-1]
+
+        # slower solution
+        def kthSmallest(self, root, k):
+            """
+            :type root: TreeNode
+            :type k: int
+            :rtype: int
+            """
+            import heapq
+            collection = []
+
+            def helper(node):
+                if node is None:
+                    return
+                helper(node.left)
+                helper(node.right)
+                collection.append(node.val)
+
+            helper(root)
+            heapq.heapify(collection)
+            return heapq.nsmallest(k, collection)[-1]
