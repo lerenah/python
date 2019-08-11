16e6231eb4d3ebb6a4db8653c8b498c9ddf65103
Lerena Holloway <holloway.lerena@gmail.com>
2019-08-10 22:22:39 -0400

    updates pal

--- weekly/longest_palindromic_sub.py
+++ weekly/longest_palindromic_sub.py
@@ -32,3 +32,27 @@ def longestPalindrome(self, s):
               if s[left_curr] != s[right_curr]:
                   matched = False
   return output
+
+
+  # alternative
+  def longestPalindrome(s):
+    if s is '':
+        return s
+    max_len = 0
+    start, end = 0, 0
+    for i in range(len(s)):
+        len1 = expandFromCenter(s, i, i)
+        len2 = expandFromCenter(s, i, i + 1)
+        l = max(len1, len2)
+        if l > end - start:
+            start = i - (l - 1) // 2
+            end = i + l // 2
+
+    return s[start:end + 1]
+
+
+def expandFromCenter(s, idx1, idx2):
+    while idx1 >= 0 and idx2 < len(s) and s[idx1] == s[idx2]:
+        idx1 -= 1
+        idx2 += 1
+    return idx2 - idx1 - 1
