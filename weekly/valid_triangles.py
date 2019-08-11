2bba76c89702657160de5a745e5d52da5248c0a7
Lerena Holloway <holloway.lerena@gmail.com>
2019-08-11 19:40:28 -0400

    updates valid triangle

--- weekly/valid_triangles.py
+++ weekly/valid_triangles.py
@@ -1,19 +1,38 @@
 def triangleNumber(self, nums):
-  """
-  :type nums: List[int]
-  :rtype: int
-  """
-  copy = []
-  copy = copy + nums
-  triangles = 0
-  sets = []
-  for el in nums:
-      copy.remove(el)
-      i = 0
-      if i + 1 in range(len(copy)):
-          if el < (copy[i] + copy[i + 1]) and copy[i] < (el + copy[i + 1]) and copy[i + 1] < (el + copy[i]):
-              sets.append([copy[i + 1], el, copy[i]])
-              triangles += 1
-      copy = []
-      copy = copy + nums
-  return triangles
+    """
+    :type nums: List[int]
+    :rtype: int
+    """
+    copy = []
+    copy = copy + nums
+    triangles = 0
+    sets = []
+    for el in nums:
+        copy.remove(el)
+        i = 0
+        if i + 1 in range(len(copy)):
+            if el < (copy[i] + copy[i + 1]) and copy[i] < (el + copy[i + 1]) and copy[i + 1] < (el + copy[i]):
+                sets.append([copy[i + 1], el, copy[i]])
+                triangles += 1
+        copy = []
+        copy = copy + nums
+    return triangles
+
+    # alternative solution
+
+    def triangleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums.sort()
+        res = 0
+        for i, num in enumerate(nums[:-2]):
+            k = i + 2
+            for j in range(i + 1, len(nums) - 1):
+                if nums[i] == 0:
+                    continue
+                while k < len(nums) and (nums[i] + nums[j]) > nums[k]:
+                    k += 1
+                res += k - j - 1
+        return res
