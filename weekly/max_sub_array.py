0f83804c4570e6f86f2d669ad91f5981d36398cc
Lerena Holloway <holloway.lerena@gmail.com>
2019-08-12 12:17:37 -0400

    updates counter

--- weekly/max_sub_array.py
+++ weekly/max_sub_array.py
@@ -16,3 +16,20 @@ def maxSubArray(self, nums):
                 highest_seen = counter
 
         return highest_seen
+
+# update
+def maxSubArray(self, nums):
+    """
+    :type nums: List[int]
+    :rtype: int
+    """
+    counter = nums[0]
+    highest_seen = nums[0]
+    for end in range(1, len(nums)):
+        # check two conditions here; a is higher or a + b is higher
+        counter = max(counter + nums[end], nums[end])
+        # update highest seen
+        if highest_seen < counter:
+            highest_seen = counter
+
+    return highest_seen
