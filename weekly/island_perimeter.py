1bb81ce99ad03c8aca41a6ac982628f7f19b3f26
Lerena Holloway <holloway.lerena@gmail.com>
2019-08-11 22:46:16 -0400

    island perimeter

---
+++ weekly/island_perimeter.py
@@ -0,0 +1,16 @@
+def islandPerimeter(self, grid):
+  """
+  :type grid: List[List[int]]
+  :rtype: int
+  """
+
+  res = 0
+  for row in range(len(grid)):
+      for column in range(len(grid[0])):
+          if grid[row][column]:
+              res += 4
+              if row and grid[row - 1][column]:
+                  res -= 2
+              if column and grid[row][column - 1]:
+                  res -= 2
+  return res
