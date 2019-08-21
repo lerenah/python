d2e72a5719c0fc9aec7ad5db401bb388a4e8097e
Lerena Holloway <holloway.lerena@gmail.com>
2019-08-20 21:09:13 -0400

    updates challenge

--- weekly/find_happy_num.py
+++ weekly/find_happy_num.py
@@ -21,3 +21,23 @@ def find_happy_number(num):
 
 
   return True
+
+  # alternative solution
+
+  def find_happy_number(num):
+    slow, fast = num, num
+    while True:
+      slow = find_square_sum(slow)  # move one step
+      fast = find_square_sum(find_square_sum(fast))  # move two steps
+      if slow == fast:  # found the cycle
+        break
+    return slow == 1  # see if the cycle is stuck on the number '1'
+
+
+  def find_square_sum(num):
+    _sum = 0
+    while (num > 0):
+      digit = num % 10
+      _sum += digit * digit
+      num //= 10
+    return _sum
