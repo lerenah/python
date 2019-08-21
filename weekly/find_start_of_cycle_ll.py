fc2cdd300ea08cfe95bb901693ad5b905a1832d0
Lerena Holloway <holloway.lerena@gmail.com>
2019-08-21 11:09:00 -0400

    updates

--- weekly/find_start_of_cycle_ll.py
+++ weekly/find_start_of_cycle_ll.py
@@ -9,3 +9,17 @@ def find_cycle_start(head):
     fast = fast.next.next
     slow = slow.next
   return head
+
+
+def has_cycle(head):
+  # TODO: Write your code here
+  if head is None:
+    return False
+  slow = head
+  fast = head
+  while fast is not None and fast.next is not None:
+    slow = slow.next
+    fast = fast.next.next
+    if slow == fast:
+      return True
+  return False
